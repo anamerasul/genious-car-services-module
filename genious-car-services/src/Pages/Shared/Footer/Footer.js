d0bb704ecdc2c400a597ac444a7ad1c3cca974a0
anamerasul <anamerasul@gmail.com>
2022-04-16 04:51:37 +0600

    Create Project Component setup and router setup

--- genious-car-services/src/Pages/Shared/Footer/Footer.js
+++ genious-car-services/src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import "./Footer.css"
+// import React, { useEffect, useState } from 'react';
+// import "./Footer.css"
 
+// const Footer = () => {
+//     const [date, setDate] = useState();
+
+//     const getYear = () => setDate(new Date().getFullYear())
+
+//     useEffect(() => {
+//         getYear()
+//     }, [])
+//     return (
+//         <footer>
+//             <p><small>&copy;  tonmoy {date}</small></p>
+//         </footer>
+//     );
+// };
+
+// export default Footer;
+
+
+
+import React, { useEffect, useState } from 'react';
+import { BsFacebook, BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
 const Footer = () => {
+
     const [date, setDate] = useState();
 
     const getYear = () => setDate(new Date().getFullYear())
@@ -10,8 +32,41 @@ const Footer = () => {
         getYear()
     }, [])
     return (
-        <footer>
-            <p><small>&copy;  tonmoy {date}</small></p>
+        <footer className='bg-gray-900  py-4 mt-8 md:mt-24'>
+            <div className='max-w-screen-xl mx-auto px-4'>
+                <div className=' flex max-w-xs mx-auto items-center justify-evenly md:justify-between'>
+                    <a
+                        href='https://facebook.com/sparkgears'
+                        className='text-white hover:text-gray-800 transition-colors duration-200'
+                    >
+                        <BsFacebook className='text-xl hover:text-blue-800 transition-colors duration-200'></BsFacebook>
+                    </a>
+                    <a
+                        href='https://twitter.com/MdChowdhuray'
+                        className='text-white  hover:text-gray-800 transition-colors duration-200'
+                    >
+
+                        <BsTwitter className='text-xl hover:text-blue-500 transition-colors duration-200'></BsTwitter>
+                    </a>
+                    <a
+                        href='https://github.com/anamerasul'
+                        className='text-white hover:text-gray-200 transition-colors duration-200'
+                    >
+                        <BsGithub className='text-xl hover:text-gray-200 transition-colors duration-200'></BsGithub>
+                    </a>
+                    <a
+                        href='https://www.linkedin.com/in/anamerasul/'
+                        className='text-white hover:text-gray-800 transition-colors duration-200'
+                    >
+                        <BsLinkedin className='text-xl hover:text-blue-500 transition-colors duration-200'></BsLinkedin>
+                    </a>
+
+
+                </div>
+                <div className='text-center text-white pt-4 sm:pt-8 font-light flex items-center justify-center'>
+                    All Rights Reserved By &copy; Geneous car | {date}
+                </div>
+            </div>
         </footer>
     );
 };
--- genious-car-services/src/Pages/Shared/Header/Header.js
+++ genious-car-services/src/Pages/Shared/Header/Header.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdLegendToggle } from 'react-icons/md';
 import './Header.css'
+import './Header.css'
+import CustomLink from '../../../Components/CustomLink/CustomLink';
 const Header = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
     return (
         <header>
-            <h2>Header</h2>
-            <h1 className="text-3xl font-bold underline text-orange-500">
-                Hello world!
-            </h1>
+            <>
+                <nav className="flex flex-wrap items-center justify-between px-2 py-3 top-0 bg-gray-800 from-yellow-700 shadow-lg  text-white mb-3">
+                    <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                        <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
+                            <Link to='/'
+                                className="text-xl md:text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-fuchsia-300 md:text-yellow-600 "
+                            >
+                                Geneous-car
+                            </Link>
+                            <button
+                                className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
+                                type="button"
+                                onClick={() => setNavbarOpen(!navbarOpen)}
+                            >
+                                <MdLegendToggle ></MdLegendToggle>
+                            </button>
+                        </div>
+                        <div
+                            className={
+                                "md:flex flex-grow items-center" +
+                                (navbarOpen ? " flex" : " hidden")
+                            }
+                            id="example-navbar-danger"
+                        >
+                            <ul className="flex flex-col md:flex-row list-none  md:ml-auto">
+                                <li className="nav-item">
+                                    <CustomLink className="px-3 py-2 flex items-center uppercase" to='/'>HOME</CustomLink>
+                                </li>
+                                <li className="nav-item">
+                                    <CustomLink className="px-3 py-2 flex items-center uppercase" to='/reviews'>Review</CustomLink>
+                                </li>
+                                <li className="nav-item">
+                                    <CustomLink className="px-3 py-2 flex items-center uppercase" to='/dashboard'>DASHBOARD</CustomLink>
+                                </li>
+                                <li className="nav-item">
+                                    <CustomLink className="px-3 py-2 flex items-center uppercase" to='/blogs'>Blogs</CustomLink>
+                                </li>
+                                <li className="nav-item">
+                                    <CustomLink className="px-3 py-2 flex items-center uppercase" to='/about'>About</CustomLink>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </>
         </header>
     );
 };
