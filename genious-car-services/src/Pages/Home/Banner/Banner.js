f82932e861b0a3dfbb47dc2be585b2485c70681c
anamerasul <anamerasul@gmail.com>
2022-04-16 19:44:54 +0600

    React Bootstrap for responsive and image slider, banner

---
+++ genious-car-services/src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- genious-car-services/src/Pages/Home/Home/Home.js
+++ genious-car-services/src/Pages/Home/Home/Home.js
@@ -3,11 +3,13 @@ import Experts from '../Experts/Experts';
 
 import Services from './../Services/Services';
 
+import Banner from './../Banner/Banner.js'
+
 const Home = () => {
     return (
         <div>
             {/* <h2>This is home</h2> */}
-
+            <Banner></Banner>
             <Services></Services>
 
             <Experts></Experts>
